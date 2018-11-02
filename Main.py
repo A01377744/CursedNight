9ee3e538bb06189bcc7a1c38fdf0067f1e384062
A01377744 <42352935+A01377744@users.noreply.github.com>
2018-11-01 18:27:05 -0600

    Update

--- Main.py
+++ Main.py
@@ -5,11 +5,14 @@ ANCHO = 800
 ALTO = 600
 
 BLANCO = (255, 255, 255)
-ROJO = (255, 0, 0)
+ROJO = (100, 0, 0)
 AZUL_OSCURO = (0, 0, 50)
 NEGRO = (1, 1, 1)
 
-estadoPersonaje = {0: (680, 900, 110, 118), 1: (660, 480, 230, 95), 2: (570, 230, 60, 120)}
+animacionCorrer = {0: (400, 900, 80, 118), 1: (480, 900, 90, 118), 2: (570, 900, 110, 118),
+                   3: (680, 900, 110, 118), 4: (790, 900, 110, 118), 5: (900, 900, 110, 118), 6: (1010, 900, 110, 118)}
+animacionAtaque = {0: (660, 480, 230, 95)}
+animacionSalto = {0: (570, 230, 60, 120)}
 
 TITULO = 0
 MENU = 1
@@ -22,6 +25,19 @@ CORRIENDO = 4
 ATTACK = 5
 
 
+def dibujarPersonaje(ventana, actualizaciones, alturaPersonaje, accion):
+    imgPersonaje = pygame.image.load('13974.gif')
+    if accion == CORRIENDO:
+        correr = actualizaciones % 7
+        imgPersonaje.set_clip(pygame.Rect(animacionCorrer[correr]))
+        imgPersonaje = imgPersonaje.subsurface(imgPersonaje.get_clip())
+        ventana.blit(imgPersonaje, (0, alturaPersonaje-116))
+    elif accion == SALTO:
+        imgPersonaje.set_clip(pygame.Rect(animacionSalto[0]))
+        imgPersonaje = imgPersonaje.subsurface(imgPersonaje.get_clip())
+        ventana.blit(imgPersonaje, (0, alturaPersonaje - 116))
+
+
 def dibujarEsqueleto(ventana, listaEnemigos):
     # VISITAR a cada elemento
     for enemigo in listaEnemigos:
@@ -29,17 +45,20 @@ def dibujarEsqueleto(ventana, listaEnemigos):
 
 
 def moverEnemigos(listaEnemigos):
-    for enemigo in listaEnemigos:
-        enemigo.rect.left -= 6
+    for k in range(len(listaEnemigos)-1, -1, -1):
+        enemigo = listaEnemigos[k]
+        enemigo.rect.left -= 11
+        if enemigo.rect.left < -100:
+            listaEnemigos.remove(enemigo)
+            break
 
 
 def dibujarSuelo(ventana, imgSuelo, xSuelo):
-    for renglon in range(2):
-        for columna in range(14):
-            xUpdate = (64 * columna)-xSuelo
-            while not xUpdate > -64:
-                xUpdate = xUpdate + 864
-            ventana.blit(imgSuelo, (xUpdate, 500+(64*renglon)))
+    for columna in range(2):
+        xUpdate = (800 * columna)-xSuelo
+        while not xUpdate > -800:
+            xUpdate = xUpdate + 1600
+        ventana.blit(imgSuelo, (xUpdate, 500))
 
 
 def dibujarBarda(ventana, imgBarda, xSuelo):
@@ -58,6 +77,16 @@ def moverArboles(ventana, imgBosque1, xBosque1):
         ventana.blit(imgBosque1, (xUpdate, 240))
 
 
+def dibujarHUD(ventana):
+    pygame.font.init()
+    fuente = pygame.font.Font(None, 50)
+    player = fuente.render('Player', 1, (255, 255, 255))
+    enemy = fuente.render('Enemy', 1, (255, 255, 255))
+    ventana.blit(player, (10, 10))
+    ventana.blit(enemy, (10, 50))
+
+
+
 def dibujar():
     pygame.init()
     ventana = pygame.display.set_mode((ANCHO, ALTO))
@@ -70,7 +99,7 @@ def dibujar():
 
     # Parametros personaje
     imgPersonaje = pygame.image.load('13974.gif')
-    imgPersonaje.set_clip(pygame.Rect(estadoPersonaje[0]))
+    imgPersonaje.set_clip(pygame.Rect(680, 900, 110, 118))
     imgPersonaje = imgPersonaje.subsurface(imgPersonaje.get_clip())
     spritePersonaje = pygame.sprite.Sprite()
     spritePersonaje.image = imgPersonaje
@@ -83,11 +112,11 @@ def dibujar():
     imgEnemigos = pygame.image.load('dracula-sprite-transparent-png-5.gif')
     imgEnemigos.set_clip(pygame.Rect(esqueleto[-1]))
     imgEnemigos = imgEnemigos.subsurface(imgEnemigos.get_clip())
-    for k in range(1):
+    for k in range(20):
         spriteEnemigo = pygame.sprite.Sprite()
         spriteEnemigo.image = imgEnemigos
         spriteEnemigo.rect = imgEnemigos.get_rect()
-        spriteEnemigo.rect.left = random.randint(800, 1000)
+        spriteEnemigo.rect.left = random.randint(800, 4000)
         spriteEnemigo.rect.bottom = 500
         listaEnemigos.append(spriteEnemigo)
 
@@ -96,8 +125,7 @@ def dibujar():
     imgBosque1 = pygame.image.load('Bosque1.png')
     imgBosque2 = pygame.image.load('Bosque2.png')
     imgTexturas = pygame.image.load('textures.png')
-    imgTexturas.set_clip(pygame.Rect(131, 231, 64, 65))
-    imgSuelo = imgTexturas.subsurface(imgTexturas.get_clip())
+    imgSuelo = pygame.image.load('Floor.jpg')
     imgTexturas.set_clip(pygame.Rect(197, 1, 32, 64))
     imgBarda = imgTexturas.subsurface(imgTexturas.get_clip())
 
@@ -108,6 +136,7 @@ def dibujar():
 
     frame = 0
     estado = TITULO
+    actualizaciones = 1
     accion = CORRIENDO
 
     # Input
@@ -119,12 +148,9 @@ def dibujar():
                 if evento.key == pygame.K_UP:
                     if spritePersonaje.rect.bottom == 500:
                         moviendo = SALTO
+                        accion = SALTO
                 elif evento.key == pygame.K_SPACE:
                         accion = ATTACK
-
-            elif evento.type == pygame.KEYUP:
-                if evento.key == pygame.K_UP:
-                    moviendo = ABAJO
 
         ventana.fill(NEGRO)
         if estado == JUGANDO:
@@ -135,27 +161,31 @@ def dibujar():
             elif moviendo == ABAJO:
                 if spritePersonaje.rect.bottom != 500:
                     spritePersonaje.rect.bottom += 10
-            elif accion == ATTACK:
-                ventana.blit(spritePersonaje.image, spritePersonaje.rect)
-
+                    if spritePersonaje.rect.bottom == 500:
+                        accion = CORRIENDO
             ventana.blit(imgFondo, (250, 0))
             ventana.blit(imgBosque2, (xBosque2, 100))
 
-            xBosque1 += 1.5
+            xBosque1 += 2
             xBosque2 -= 0.2
-            xSuelo += 5
+            xSuelo += 10
             frame += 1
+            actualizaciones += 1
             moverEnemigos(listaEnemigos)
             moverArboles(ventana, imgBosque1, xBosque1)
-            dibujarBarda(ventana, imgBarda, xSuelo)
             dibujarEsqueleto(ventana, listaEnemigos)
             dibujarSuelo(ventana, imgSuelo, xSuelo)
-            ventana.blit(spritePersonaje.image, spritePersonaje.rect)
             pygame.draw.line(ventana, NEGRO, (0, 500), (800, 500), 2)
+            alturaPersonaje = spritePersonaje.rect.bottom
+            dibujarPersonaje(ventana, actualizaciones, alturaPersonaje, accion)
+            dibujarHUD(ventana)
         if estado == TITULO:
+            x = random.randint(0, 50)
+            pygame.draw.rect(ventana, (x, x, x), (0, 0, 800, 600))
             ventana.blit(imgLogo, (187, 167))
             if accion == ATTACK:
                 estado = JUGANDO
+                accion = CORRIENDO
 
         pygame.display.flip()
         reloj.tick(40)
